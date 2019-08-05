c3161c5dfc099917b916bd691cb258e73e81c450
ExpDev07 <mariusri@ostfoldfk.no>
2019-08-05 07:54:31 +0200

    latest

--- legacy-rp-admin-frontend/app/routes/application.js
+++ legacy-rp-admin-frontend/app/routes/application.js
@@ -1,4 +1,5 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
+// Keep all routes unless explicitly stated otherwise authenticated for now.
 export default Route.extend(AuthenticatedRouteMixin);
