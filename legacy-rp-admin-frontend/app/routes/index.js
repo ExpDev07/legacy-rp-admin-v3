b4a3858031127af0b1f4e251f3dda1a5b80635f8
ExpDev07 <mariusri@ostfoldfk.no>
2019-08-07 11:15:03 +0200

    auth works

--- legacy-rp-admin-frontend/app/routes/index.js
+++ legacy-rp-admin-frontend/app/routes/index.js
@@ -1,15 +1,4 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Route.extend(AuthenticatedRouteMixin, {
-
-    actions: {
-        // run this action every x seconds.
-        updateModel() {
-
-            this.store.query('log', {}).then((logs) => this.model = logs);
-    
-        }
-    }
-
-});
+export default Route.extend(AuthenticatedRouteMixin);
