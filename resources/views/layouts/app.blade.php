bd8508ea1e3d237ec0dfdab2631e6f38e0aeeb5f
ExpDev07 <marius.richardsen@hiof.no>
2019-09-27 16:46:39 +0200

    frontend compiled

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Page title -->
+    <title>Legacy RP - Admin</title>
+
+    <!-- Pull library css -->
+
+</head>
+
+<body>
+
+    <!-- Pull in library scripts -->
+</body>
+
+
