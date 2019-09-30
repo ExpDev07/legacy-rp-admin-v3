dad9085fda998db578439c27c2031b77920c5b72
ExpDev07 <marius.richardsen@hiof.no>
2019-10-01 01:34:44 +0200

    better error msg

--- app/Http/Middleware/StaffMiddleware.php
+++ app/Http/Middleware/StaffMiddleware.php
@@ -25,7 +25,9 @@ class StaffMiddleware
         // Check for staff status.
         if (!($this->isStaff($request))) {
             auth()->logout(); // log them out also.
-            return redirect()->route('login')->with('error', 'You must be a staff member to use this webpanel!');
+            return redirect()->route('login')->with('error',
+                'You must be a staff member to use this webpanel! If you believe this is a mistake, contact a developer.'
+            );
         }
         return $next($request);
     }
