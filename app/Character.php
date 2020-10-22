e442eb79e841c359185484a7ed365302589d2d29
ExpDev07 <marius.richardsen@hiof.no>
2020-10-23 01:13:35 +0200

    optimized imports

--- app/Character.php
+++ app/Character.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Support\Str;
 
 /**
  * A character made by a player.
