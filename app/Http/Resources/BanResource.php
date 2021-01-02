2d07828602015e2c6bc56032552d48a312566e1b
ExpDev07 <marius.richardsen@hiof.no>
2021-01-02 10:20:13 +0100

    optimize imports

--- app/Http/Resources/BanResource.php
+++ app/Http/Resources/BanResource.php
@@ -4,7 +4,6 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Support\Facades\Date;
 
 class BanResource extends JsonResource
 {
