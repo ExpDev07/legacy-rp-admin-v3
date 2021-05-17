9214fd4193cbcad0f88ac7e7dbec7639f690f6ab
Marius <ExpDev07@users.noreply.github.com>
2021-05-17 19:43:29 +0200

    Update formatSeconds.js

--- resources/js/Filters/formatSeconds.js
+++ resources/js/Filters/formatSeconds.js
@@ -1,6 +1,6 @@
 import moment from 'moment';
 
 // Formatting seconds to human readable time
-export default function(value) {
+export default formatSeconds(value) {
     return moment.duration(parseInt(value), "seconds").humanize();
-});
+};
