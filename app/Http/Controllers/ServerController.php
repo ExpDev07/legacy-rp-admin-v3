8ef590705a8833be3c835a5fe987d18662396735
Milan <milanbartky@gmail.com>
2021-06-09 17:20:47 +0200

    [TESTED] View Server page which shows all online players

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ServerStoreRequest;
+use App\Http\Resources\PlayerResource;
 use App\Http\Resources\ServerResource;
+use App\Player;
 use App\Server;
+use GuzzleHttp\Client;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -48,6 +51,47 @@ class ServerController extends Controller
     {
         $server->forceDelete();
         return back()->with('success', 'The server was successfully removed from tracking.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Server $server
+     * @return Response
+     */
+    public function show(Server $server): Response
+    {
+        $players = [null];
+        $url = env('OP_FW_SERVER');
+
+        if ($url) {
+            try {
+                $client = new Client();
+                $res = $client->request('GET', 'https://' . $url . '/op-framework/connections.json');
+
+                $response = json_decode($res->getBody()->getContents(), true);
+                if (!empty($response) && !empty($response['joining']) && !empty($response['joined'])) {
+                    $steamIdentifiers = array_values(array_merge(
+                        array_map(function($player) {
+                            return $player['steamIdentifier'];
+                        }, $response['joining']['players']),
+                        array_map(function($player) {
+                            return $player['steamIdentifier'];
+                        }, $response['joined']['players'])
+                    ));
+
+                    $query = Player::query()->orderBy('last_connection');
+                    $query->whereIn('steam_identifier', $steamIdentifiers);
+
+                    $players = PlayerResource::collection($query->get());
+                }
+            } catch (\Throwable $e) {}
+        }
+
+        return Inertia::render('Servers/Show', [
+            'server' => new ServerResource($server),
+            'players' => $players
+        ]);
     }
 
 }
