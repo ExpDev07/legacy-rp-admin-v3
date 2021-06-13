7e8303871505918589694b0fbc082b7c7d8a1661
Milan <milanbartky@gmail.com>
2021-06-13 19:24:42 +0200

    [TESTED] Add descriptions for publix garages (previously they were only numbers)

--- app/Http/Resources/VehicleResource.php
+++ app/Http/Resources/VehicleResource.php
@@ -18,7 +18,7 @@ class VehicleResource extends JsonResource
     {
         return [
             'id'          => $this->vehicle_id,
-            'garage_name' => $this->garage_identifier,
+            'garage_name' => $this->garage(),
             'model_name'  => vehicle_model_name($this->model_name),
             'plate'       => $this->plate,
         ];
