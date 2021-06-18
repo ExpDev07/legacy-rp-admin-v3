ccbc7b8a86e94671d61bc4d2f917dbe7f98eb961
Milan <milanbartky@gmail.com>
2021-06-19 00:32:33 +0200

    - Update all migrations

---
+++ database/migrations/2021_06_19_003105_create_mdt_warrants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMdtWarrantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mdt_warrants', function (Blueprint $table) {
+            $table->integer('id')->nullable(false)->autoIncrement();
+            $table->integer('character_id')->nullable()->default(null);
+            $table->longText('character_name')->nullable()->default(null);
+            $table->integer('timestamp')->nullable()->default(null);
+            $table->index('character_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mdt_warrants');
+    }
+}
