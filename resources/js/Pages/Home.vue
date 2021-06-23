fdb776ae9038aa7b8aeeba8c7c9385b7f69fb171
Milan <milanbartky@gmail.com>
2021-06-23 21:07:47 +0200

    - player count gauge moved down a bit

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -18,7 +18,7 @@
                 </span>
             </div>
 
-            <div class="flex mt-7">
+            <div class="flex mt-14">
                 <div class="p-4 bg-gray-100 shadow-lg dark:bg-gray-700 dark:text-gray-100 flex justify-between">
                     <vue-circle ref="serverCount"
                         :progress="playerCountPercentage()"
