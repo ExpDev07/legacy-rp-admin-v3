bf2033921a6dc4ea876164850e0f06245a8acc48
Milan <milanbartky@gmail.com>
2021-08-31 02:07:45 +0200

    - Changelog show which update is active and which ones are coming soon

--- resources/js/Pages/Changelog.vue
+++ resources/js/Pages/Changelog.vue
@@ -11,8 +11,16 @@
         </portal>
 
         <div class="-mt-6">
-            <div v-for="update in updates" :key="update.title" class="pb-3 mb-3 border-b-2 border-gray-800 dark:border-gray-500">
-                <h2>{{ update.title }}</h2>
+            <div v-for="update in updates.updates" :key="update.title" class="pb-3 mb-3 border-b-2 border-gray-800 dark:border-gray-500" :class="{ 'opacity-40' : updates.current < update.id }">
+                <h2 :class="{ 'text-green-500' : updates.current === update.id }">
+                    {{ update.title }}
+                    <sup v-if="updates.current === update.id" class="text-sm">
+                        {{ t('changelog.current') }}
+                    </sup>
+                    <sup v-else-if="updates.current < update.id" class="text-sm">
+                        {{ t('changelog.soon') }}
+                    </sup>
+                </h2>
                 <p class="text-sm text-muted dark:text-dark-muted mb-2">{{ update.time | formatTime }}</p>
                 <ul>
                     <li v-for="li in update.body" :key="li" class="ml-3 pl-2 list-dash">
@@ -32,7 +40,7 @@ export default {
     layout: Layout,
     props: {
         updates: {
-            type: Array,
+            type: Object,
             required: true
         }
     },
