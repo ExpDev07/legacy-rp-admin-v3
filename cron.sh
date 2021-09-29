d9259f812a4a90114da7c1836b21f37aaaef63c7
Milan <milanbartky@gmail.com>
2021-09-29 22:41:23 +0200

    fix cronjob .sh

--- cron.sh
+++ cron.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Path to your instance without trailing slash
+rootDir=/var/www/legacy-rp-admin-v3
+
 # $1 is the cluster (c3, c2, etc.)
 # $2 is the dev-token
 runCronJobs() {
@@ -18,17 +21,17 @@ runCronJobs "c3" "my-dev-token"
 
 
 # Automatically delete log files older than 14 days
-before=$(countFiles ./storage/logs/)
+before=$(countFiles $rootDir/storage/logs/)
 
-find ./storage/logs/ -mindepth 1 -type f -mtime +14 -delete
+find $rootDir/storage/logs/ -mindepth 1 -type f -mtime +14 -delete
 
-after=$(countFiles ./storage/logs/)
+after=$(countFiles $rootDir/storage/logs/)
 echo "Removed "$(($before - $after))" log files"
 
 # Automatically delete sessions older than 2 days
-before=$(countFiles ./storage/framework/session_storage)
+before=$(countFiles $rootDir/storage/framework/session_storage)
 
-find ./storage/framework/session_storage -mindepth 1 -type f -mtime +2 -delete
+find $rootDir/storage/framework/session_storage -mindepth 1 -type f -mtime +2 -delete
 
-after=$(countFiles ./storage/framework/session_storage)
+after=$(countFiles $rootDir/storage/framework/session_storage)
 echo "Removed "$(($before - $after))" .session files"
