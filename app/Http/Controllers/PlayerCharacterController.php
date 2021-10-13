6e8b521293ff532c0b7929939508ee780e928c06
Milan <milanbartky@gmail.com>
2021-10-13 21:12:35 +0200

    small fix

--- app/Http/Controllers/PlayerCharacterController.php
+++ app/Http/Controllers/PlayerCharacterController.php
@@ -580,7 +580,7 @@ class PlayerCharacterController extends Controller
     public function export(Character $character, Request $request): \Illuminate\Http\Response
     {
         $export = [
-            $character->first_name . ' ' . $character->last_name,
+            '**' . $character->first_name . ' ' . $character->last_name . '**',
             'DOB: - ' . $character->date_of_birth,
             'CID: - ' . $character->character_id,
             'Phone Number: - ' . $character->phone_number,
@@ -599,21 +599,29 @@ class PlayerCharacterController extends Controller
         $export[] = '';
 
         // Export Vehicles
-        $export[] = 'Vehicles';
+        $export[] = '**Vehicles**';
 
         $vehicles = $character->vehicles()->get();
         foreach ($vehicles as $vehicle) {
             $export[] = $vehicle->model_name . ' - ' . $vehicle->plate . ' - ' . $vehicle->garage();
         }
 
+        if (empty($vehicles)) {
+            $export[] = 'N/A';
+        }
+
         $export[] = '';
 
         // Export Properties
-        $export[] = 'Houses';
+        $export[] = '**Houses**';
 
         $properties = $character->properties()->get();
         foreach ($properties as $property) {
             $export[] = $property->property_address . ' - ' . $property->companyName();
+        }
+
+        if (empty($properties)) {
+            $export[] = 'N/A';
         }
 
         return self::text(200, implode(PHP_EOL, $export));
