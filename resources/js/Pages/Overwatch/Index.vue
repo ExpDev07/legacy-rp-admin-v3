112305913440d1bf67c70c42792a3ae9bfea19a6
Milan <milanbartky@gmail.com>
2021-10-15 02:29:35 +0200

    - Some optimizations to the Overwatch feature

--- resources/js/Pages/Overwatch/Index.vue
+++ resources/js/Pages/Overwatch/Index.vue
@@ -10,24 +10,10 @@
             </p>
         </portal>
 
-        <portal to="actions">
-            <button class="px-4 py-2 text-sm font-semibold text-white bg-indigo-600 rounded dark:bg-indigo-400" type="button" @click="refresh">
-                <span v-if="!isLoading">
-                    <i class="mr-1 fa fa-refresh"></i>
-                    {{ t('global.refresh') }}
-                </span>
-
-                <span v-else>
-                    <i class="mr-1 fa fa-refresh animate-spin"></i>
-                    {{ t('global.loading') }}
-                </span>
-            </button>
-        </portal>
-
         <ScreenshotAttacher :close="screenshotAttached" :steam="screenshot.steam" :url="screenshot.url" v-if="isAttaching" />
 
-        <v-section>
-            <div v-if="screenshot">
+        <v-section class="-mt-10 max-w-screen-lg">
+            <div v-if="screenshot" class="-mt-8">
                 <div class="flex justify-between">
                     <div class="flex">
                         <inertia-link
@@ -42,13 +28,29 @@
                         </badge>
                     </div>
 
-                    <button class="px-5 py-2 font-semibold text-white bg-primary rounded dark:bg-dark-primary" @click="isAttaching = true">
-                        <i class="fas fa-paper-plane mr-1"></i>
-                        {{ t('screenshot.title') }}
-                    </button>
+                    <div class="flex">
+                        <button class="px-5 py-2 font-semibold text-white bg-primary rounded dark:bg-dark-primary mr-3" @click="isAttaching = true">
+                            <i class="fas fa-paper-plane mr-1"></i>
+                            {{ t('screenshot.title') }}
+                        </button>
+
+                        <button class="px-5 py-2 font-semibold text-white bg-indigo-600 rounded dark:bg-indigo-400" @click="refresh">
+                            <span v-if="!isLoading">
+                                <i class="mr-1 fa fa-refresh"></i>
+                                {{ t('global.refresh') }}
+                            </span>
+
+                            <span v-else>
+                                <i class="mr-1 fa fa-refresh animate-spin"></i>
+                                {{ t('global.loading') }}
+                            </span>
+                        </button>
+                    </div>
                 </div>
 
-                <img :src="screenshot.url" alt="Screenshot" class="mt-3 block" />
+                <a :href="screenshot.url" class="mt-5 block" target="_blank">
+                    <img :src="screenshot.url" alt="Screenshot" class="block" />
+                </a>
             </div>
             <p v-if="screenshotError" class="font-semibold text-danger dark:text-dark-danger">{{ screenshotError }}</p>
         </v-section>
