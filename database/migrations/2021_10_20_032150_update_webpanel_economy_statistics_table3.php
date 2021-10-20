469fedea07f5643509fa2247786e8b821cd259a5
Milan <milanbartky@gmail.com>
2021-10-20 03:22:52 +0200

    fix migration for real now?

--- database/migrations/2021_10_20_032150_update_webpanel_economy_statistics_table3.php
+++ database/migrations/2021_10_20_032150_update_webpanel_economy_statistics_table3.php
@@ -12,8 +12,8 @@ class UpdateWebpanelEconomyStatisticsTable3 extends Migration
      */
     public function up()
     {
-        // Delete everything before Wed Oct 20 2021 01:19:44 GMT+0000 because we added vehicles to it
-        DB::table('webpanel_economy_statistics')->where('last_updated', '<', 1634692784)->delete();
+        // Delete everything before Tue Oct 19 2021 23:25:00 GMT+0000 because we added vehicles to it
+        DB::table('webpanel_economy_statistics')->where('last_updated', '<', 1634685900)->delete();
     }
 
     /**
