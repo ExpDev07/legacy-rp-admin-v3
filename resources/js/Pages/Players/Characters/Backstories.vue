caca7924207a7863d4ce01be51906481d595226b
Twoot <milanbartky@gmail.com>
2022-03-30 12:22:49 +0200

    backstories

--- resources/js/Pages/Players/Characters/Backstories.vue
+++ resources/js/Pages/Players/Characters/Backstories.vue
@@ -6,22 +6,17 @@
                 {{ t("backstories.title") }}
             </h1>
             <p>
-                {{ t("backstories.welcome") }}
+                {{ t("backstories.description") }}
+                <a @click="refresh($event)" class="text-indigo-600 text-xs !no-underline dark:text-indigo-300 hover:text-yellow-500 dark:hover:text-yellow-300">
+                    {{ t('global.refresh') }}
+                </a>
             </p>
-        </portal>
-
-        <portal to="actions">
-            <button class="px-4 py-2 text-sm font-semibold text-white bg-indigo-600 rounded dark:bg-indigo-400"
-                    type="button" @click="refresh">
-                <i class="mr-1 fa fa-refresh"></i>
-                {{ t('global.refresh') }}
-            </button>
         </portal>
 
         <div class="flex -mt-6 justify-between max-w-screen-2xl mobile:flex-wrap">
             <div class="p-4 max-w-xl pl-6 italic border-l-4 border-gray-300 inline-block bg-gray-100 shadow-lg dark:border-gray-500 dark:bg-gray-700 dark:text-gray-100 mobile:w-full mobile:mb-3">
-                <span class="mb-1 block">
-                    {{ character.backstories }}
+                <span class="mb-1 block text-sm">
+                    {{ character.backstory }}
                 </span>
                 <inertia-link :href="'/players/' + character.steamIdentifier + '/characters/' + character.id + '/edit'" class="text-indigo-600 text-xs !no-underline dark:text-indigo-300 hover:text-yellow-500 dark:hover:text-yellow-300">
                     - {{ character.firstName }} {{ character.lastName }}
@@ -48,7 +43,9 @@ export default {
         };
     },
     methods: {
-        refresh: async function () {
+        refresh: async function (e) {
+            e.preventDefault();
+
             if (this.isLoading) {
                 return;
             }
