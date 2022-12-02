491d0eeda4e2c479fc4dbacf360569a567ae901c
Laura <milanbartky@gmail.com>
2022-12-02 16:07:56 +0100

    better update in progress message

--- app/Http/Middleware/UpdateMiddleware.php
+++ app/Http/Middleware/UpdateMiddleware.php
@@ -14,6 +14,7 @@ use Illuminate\Http\Request;
  */
 class UpdateMiddleware
 {
+    const Style = 'body,html{margin:0;background:#111827;height:100%;width:100%}div{color:#fff;font-family:Nunito,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);font-size:28px;font-weight:600;text-shadow:0 0 4px #000;text-align:center}small{display:block;font-size:16px;font-weight:500;font-style:italic;color:#cecece}';
 
     /**
      * Handle an incoming request.
@@ -25,7 +26,9 @@ class UpdateMiddleware
     public function handle(Request $request, Closure $next)
     {
         if (file_exists(__DIR__ . '/../../../update')) {
-            abort(418, 'Update in progress');
+            http_response_code(418);
+
+            die('<style>' . self::Style . '</style><div>Update in progress<small>Try again in a few minutes</small></div>');
         }
 
         return $next($request);
