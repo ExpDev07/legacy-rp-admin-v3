c6c56becb39497073c2fdfebfeab0d36151e5ca0
Laura <milanbartky@gmail.com>
2023-04-18 13:03:53 +0200

    yes

--- routes/console.php
+++ routes/console.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\DB;
 // UPDATE `inventories` SET `item_name` = 'weapon_addon_hk416' WHERE `item_name` = 'weapon_addon_m4'
 Artisan::command('run-query {query}', function(string $query) {
 	if (!defined("HAS_CLUSTER_ARG")) {
-		$this->error('No cluster argument defined, iterating through all clusters...');
+		$this->warn('No cluster argument defined, iterating through all clusters...');
 
 		$dir = __DIR__ . '/../envs';
 
@@ -35,11 +35,11 @@ Artisan::command('run-query {query}', function(string $query) {
 
 			$command = 'php ' . __DIR__ . '/../artisan run-query --cluster=' . $cluster . ' ' . json_encode($query);
 
-			$this->debug(' - ' . $command);
+			$this->comment(' - ' . $command);
 
 			$output = shell_exec($command);
 
-			$this->debug('Output: ' . $output);
+			$this->comment($output);
 		}
 
 		return;
