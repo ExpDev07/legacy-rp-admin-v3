81b03f6ceaaaa697ab8df6f0e867be64119f8e9b
Laura <milanbartky@gmail.com>
2023-05-03 23:50:50 +0200

    small fix

--- app/Http/Controllers/SystemController.php
+++ app/Http/Controllers/SystemController.php
@@ -12,42 +12,15 @@ class SystemController extends Controller
 		$all = DB::select("SELECT COUNT(*) AS count, SUBSTRING_INDEX(reason, '-', 2) AS reason, SUM(playtime) / COUNT(*) as playtime FROM user_bans LEFT JOIN users ON license_identifier = identifier WHERE creator_name IS NULL AND SUBSTRING_INDEX(identifier, ':', 1) = 'license' AND SUBSTRING_INDEX(reason, '-', 1) IN ('MODDING', 'INJECTION', 'NO_PERMISSIONS', 'ILLEGAL_VALUES', 'TIMEOUT_BYPASS', 'MEDIOCRE') GROUP BY SUBSTRING_INDEX(reason, '-', 2) LIMIT 20");
         $month = DB::select("SELECT COUNT(*) AS count, SUBSTRING_INDEX(reason, '-', 2) AS reason, SUM(playtime) / COUNT(*) as playtime FROM user_bans LEFT JOIN users ON license_identifier = identifier WHERE creator_name IS NULL AND SUBSTRING_INDEX(identifier, ':', 1) = 'license' AND timestamp >= " . (strtotime("-1 month")) . " AND SUBSTRING_INDEX(reason, '-', 1) IN ('MODDING', 'INJECTION', 'NO_PERMISSIONS', 'ILLEGAL_VALUES', 'TIMEOUT_BYPASS', 'MEDIOCRE') GROUP BY SUBSTRING_INDEX(reason, '-', 2) LIMIT 20");
 
-		$graph = DB::select("SELECT timestamp FROM user_bans WHERE creator_name IS NULL AND SUBSTRING_INDEX(identifier, ':', 1) = 'license' AND SUBSTRING_INDEX(reason, '-', 1) IN ('MODDING', 'INJECTION', 'NO_PERMISSIONS', 'ILLEGAL_VALUES', 'TIMEOUT_BYPASS')");
+		$graphData = $this->buildGraphData([], "SELECT timestamp FROM user_bans WHERE creator_name IS NULL AND SUBSTRING_INDEX(identifier, ':', 1) = 'license' AND SUBSTRING_INDEX(reason, '-', 1) IN ('MODDING', 'INJECTION', 'NO_PERMISSIONS', 'ILLEGAL_VALUES', 'TIMEOUT_BYPASS')");
 
-		$graphDays = [];
+        $keys = array_keys($graphData);
+        $min = $keys[0];
+        $max = $keys[sizeof($keys) - 1];
 
-		foreach($graph as $ban) {
-			$day = strtotime(date("Y-m-d", $ban->timestamp));
+		$image = $this->renderGraph(array_values($graphData), date("m/d/Y", $min) . ' - ' . date("m/d/Y", $max) . ' (7d avg)');
 
-			if(!isset($graphDays[$day])) {
-				$graphDays[$day] = 0;
-			}
-
-			$graphDays[$day]++;
-		}
-
-		$min = empty($graphDays) ? (time() - 86400 * 7) : min(array_keys($graphDays));
-		$max = strtotime(date("Y-m-d"));
-
-		$averageData = [];
-
-		for ($x = $min; $x <= $max; $x += 86400) {
-			$start = $x - (86400 * 7);
-
-			$average = 0;
-
-			for ($y = $start; $y <= $x; $y += 86400) {
-				$average += $graphDays[$y] ?? 0;
-			}
-
-			$average /= 7;
-
-			$averageData[] = $average;
-		}
-
-		$image = $this->renderGraph($averageData, date("m/d/Y", $min) . ' - ' . date("m/d/Y", $max) . ' (7d avg)');
-
-		$image = '<img src="' . $image . '" style="max-width: 100%; display: block; border: 1px solid #9CA3AF" />';
+		$image = '<img src="' . $image . '" style="max-width: 100%; display: block; border: 1px solid #9CA3AF" />' . PHP_EOL;
 
         usort($all, function ($a, $b) {
             return $b->count - $a->count;
@@ -142,5 +115,53 @@ class SystemController extends Controller
 		$image = '<img src="' . $image . '" style="max-width: 100%; display: block; border: 1px solid #9CA3AF" />';
 
 		return $this->fakeText(200, $image);
+    }
+
+    protected function buildGraphData($existingData, $query)
+    {
+        $graph = DB::select($query);
+
+        $index = sizeof($existingData);
+
+		$graphDays = [];
+
+		foreach($graph as $ban) {
+			$day = strtotime(date("Y-m-d", $ban->timestamp));
+
+			if(!isset($graphDays[$day])) {
+				$graphDays[$day] = 0;
+			}
+
+			$graphDays[$day]++;
+		}
+
+		$min = empty($graphDays) ? (time() - 86400 * 7) : min(array_keys($graphDays));
+		$max = strtotime(date("Y-m-d"));
+
+		for ($x = $min; $x <= $max; $x += 86400) {
+			$start = $x - (86400 * 7);
+
+			$average = 0;
+
+			for ($y = $start; $y <= $x; $y += 86400) {
+				$average += $graphDays[$y] ?? 0;
+			}
+
+			$average /= 7;
+
+            if (!isset($existingData[$x])) {
+                $existingData[$x] = [];
+            }
+
+            if ($index > 0) {
+                for ($i = 0; $i < $index; $i++) {
+                    $existingData[$x][$i] = $existingData[$x][$i] ?? 0;
+                }
+            }
+
+			$existingData[$x][$index] = $average;
+		}
+
+        return $existingData;
     }
 }
